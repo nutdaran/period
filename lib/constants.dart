869fe5052269fe8c734499605ba0b752871f1d28
nutdaran <daran.thw@student.mahidol.edu>
2023-02-22 23:24:47 +0700

    add theme color data

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const mainBgColor = Colors.white;
+const themeColor = Color(0xFEC9C9);
+const doneButtonColor = Color(0x44C160);
+const blackColor = Color(0x252525);
